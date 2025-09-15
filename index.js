089a041b1a5cfadfb03bb70da0015de12bd6a34b
Ткачук Ігор Віталійович <Igor.Tkachuk1@cvgas.com.ua>
2025-09-15 17:07:19 +0300

    feat: slide page markup

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-function makeGalleryItem({src,name}) {
-  const item = document.createElement('button');
+let mainContent;
+
+window.addEventListener("load", main);
+
+function makeGalleryItem({ src, name }) {
+  const item = document.createElement("button");
   item.classList.add("item");
 
-  const el = document.createElement('img');
-  src && (el.setAttribute('src', src), el.setAttribute("alt", name));
+  const el = document.createElement("img");
+  src && (el.setAttribute("src", src), el.setAttribute("alt", name));
 
   item.appendChild(el);
 
-  src || (item.style.display = 'none');
+  src || (item.style.display = "none");
 
   return item;
 }
 
-function makeGalleryItemDesc({name, artist}){
+function makeGalleryItemDesc({ name, artist }) {
   const elContainer = document.createElement("section");
   elContainer.classList.add("description");
 
@@ -38,22 +42,39 @@ async function getData() {
 async function main() {
   const data = await getData();
 
-  const container = document.querySelector("main");
+  const container = document.querySelector(".gallery");
 
   data.forEach((item) => {
     const elem = makeGalleryItem({
       src: item?.images?.thumbnail,
-      name: item?.name
+      name: item?.name,
     });
     const desc = makeGalleryItemDesc({
       name: item?.name,
-      artist: item?.artist?.name
-    })
+      artist: item?.artist?.name,
+    });
 
     elem.appendChild(desc);
 
     container.appendChild(elem);
   });
+
+  const btnStartSS = document.querySelector("header button");
+  btnStartSS.addEventListener("click", startSlideshow);
 }
 
-window.addEventListener("load", main);
+function startSlideshow({ target: btnStartSS }) {
+  // debugger;
+  let content = document.querySelector(".gallery");
+
+  if (!mainContent) {
+    btnStartSS.innerText = "stop slideshow";
+    mainContent = content.innerHTML;
+    content.innerHTML = "";
+    return;
+  }
+
+  btnStartSS.innerText = "start slideshow";
+  content.innerHTML = mainContent;
+  mainContent = "";
+}
